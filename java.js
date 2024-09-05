bd10d9d7c3f1b1e56de6e2dc5fa1b2e60289a25f
PYKE <tomas.osorno098@pascualbravo.edu.co>
2024-09-05 14:45:31 -0500

    java.js

---
+++ java.js
@@ -0,0 +1,101 @@
+//1
+let nombre = "daniel";
+
+nombre === "daniel" ? nombre+=" HOLA, ,MARIO " :nombre+=" CHAO, MARIO ";    
+
+console.log(nombre);
+//candenas de texo con condicional se utiliza += en las dos condiciones para numeros += -=
+//2
+let saludo = "buenos dias";
+
+saludo === "buenos dias" ? saludo+=" daniel como estas? " : saludo +=" no hay respuesta";
+
+console.log(saludo)
+//3
+let ID = 1012;
+
+ID === 1012 ? ID+=" ID CORRECTA ": ID+= "ID INCORRECTA";
+
+console.log(ID)
+//4
+let apellido = "gomez";
+
+apellido === "gomez" ? apellido+=" es tu apellido increible " : saludo +=" no es tu apellido ";
+
+console.log(apellido)
+//5
+let helado = "helado de chocolate";
+
+helado === "helado de chocolate ? " ? helado+=" si gracias ": helado+=" no gracias ";
+
+console.log(helado)
+//6
+let galletas = "quieren galletas";
+
+galletas === "quieren galletas" ? galletas+=" son de chocolate ? ": galletas+=" no me gustan gracias";
+
+console.log(galletas)
+//7
+let video_juegos = "jugamos algo ?";
+
+video_juegos === "quieres jugar algo ?" ? video_juegos+=" si vamos " : video_juegos+=" no me da pereza ";
+
+console.log(video_juegos)
+//8
+let color = "te gusta el rojo ?";
+
+color === "no te gusta el rojo ?" ? color+=" si es bonito " : color+= " me parece feo ";
+
+console.log(color)
+//9
+let novios = "quieres ser mi novia ?";
+
+novios === "quieres ser mi novia ?" ? novios+=" si " : novios+= " no ";
+
+console.log(novios)
+//10
+let animales = "tienes un gato naranjado ?";
+
+animales === "tienes un gato negro ?" ? animales+=" si es naranjado ": animales+= " talvez ";
+
+console.log(animales)
+
+console.log("AHORA LOS 5 EJERCICIOS CON LAS CONST Y SUS RESPECTIVAS OPERACIONES")
+//ejercicios con 5 const
+//1-------------------------------------------------------------------------------------------
+const numero_1 = 10;
+const numero_2 = 30;
+
+const respuesta = numero_1 + numero_2;
+
+console.log(" la suma de ",numero_1," y el numero ",numero_2," es ",respuesta)
+//2-------------------------------------------------------------------------------------------
+const numero_3 = 10;
+const numero_4 = 10;
+
+const respuesta2 = numero_3 - numero_4;
+
+console.log(" la resta de ",numero_3," y el numero ",numero_4," es ",respuesta2)
+//3-------------------------------------------------------------------------------------------
+const numero_5 = 10;
+const numero_6 = 10;
+
+const respuesta3 = numero_5 * numero_6;
+
+console.log(" la multiplicacion del numero ",numero_5," y el numero ",numero_6," es ",respuesta3)
+//4-------------------------------------------------------------------------------------------
+const numero_7 = 2;
+const numero_8 = 2;
+
+const respuesta4 = numero_7 / numero_8;
+
+console.log(" la division del numero ",numero_7," y el numero ",numero_8," es ",respuesta4)
+//5-------------------------------------------------------------------------------------------
+const PI = 3.14159;
+
+const radio = 5;
+const area = PI * radio * radio;
+
+console.log("Área del círculo con radio", radio, "es", area);
+
+
